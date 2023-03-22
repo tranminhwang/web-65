2809b7ebd2e9ff2e6ad44ad8615a2e7381120c0b
Hao Phan <haophan1507>
2023-03-22 21:17:03 +0700

    test

--- session_02/routers/students.js
+++ session_02/routers/students.js
@@ -1,17 +1,25 @@
 // CRUD: Create, Read, Update, Delete
-import express from "express";
+import express from 'express';
 
 const router = express.Router();
 const students = [
-  { id: 1, name: "Alice" },
+  { id: 1, name: 'Alice' },
   {
     id: 2,
-    name: "Bob",
+    name: 'Bob',
   },
 ];
 
-router.get("/:id", (req, res) => {
+//getall
+router.get('/', (req, res) => {
+  res.status(200).send(students);
+});
+
+//get one
+router.get('/:id', (req, res) => {
   console.log(req.params);
+  console.log(req.query);
+  console.log(req.body);
   // if (Object.keys(req.query).length > 0) {
   //   const { name } = req.query;
   //   res.send(students.find((student) => student.name === name));
@@ -25,11 +33,47 @@ router.get("/:id", (req, res) => {
 
   res.status(200).send(student);
 });
-router.get("/create", (req, res) => {});
-router.get("/read", (req, res) => {
-  res.send("Read students");
+
+//create
+router.post('/', (req, res) => {
+  students.push(req.body);
+  res.json(students);
 });
-router.get("/update", (req, res) => {});
-router.get("/delete", (req, res) => {});
+
+//update
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+
+  const newStudents = students.map((item) => {
+    if (item.id === +id) {
+      console.log('run');
+      return req.body;
+    }
+    return item;
+  });
+  console.log(newStudents);
+  res.json(newStudents);
+});
+
+//update
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  const item = students.filter((item) => item.id !== +id);
+  res.json(item);
+});
+
+// router.get('/create', (req, res) => {});
+// router.get('/read', (req, res) => {
+//   res.send('Read students');
+// });
+// router.get('/update', (req, res) => {});
+// router.get('/delete', (req, res) => {});
+
+//CRUD
+//Create = post
+//Update = put/patch
+//Read = get
+//Delete = delete
 
 export default router;
