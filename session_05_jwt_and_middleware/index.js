c3046f5cdaf1e0c667ec9ecb70794711e9d0152f
tranminhwang <j2team.tranminhquang@gmail.com>
2023-03-29 20:14:12 +0700

    jwt

---
+++ session_05_jwt_and_middleware/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import authRouter from "./routes/authRoutes.js";
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/v1/auth", authRouter);
+
+app.listen(8080, () => {});
